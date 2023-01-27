ec92889e8709b49db5aa96fc7430e83771e5e1b1
gravesti <isaac.gravestock@roche.com>
2023-01-27 16:22:37 +0100

    tidy up

--- OTHER_FORMATS/StatisticalEngineering.R
+++ OTHER_FORMATS/StatisticalEngineering.R
@@ -17,11 +17,12 @@ require(magick)
 # Make the Roche font available
 font_dir <- "/Library/Fonts/Roche Sans" # Mac OS
 
-font_add("RocheSans",
-         file.path(font_dir, "RocheSans-Regular.ttf"),
-         file.path(font_dir, "RocheSans-Bold.ttf"),
-         file.path(font_dir, "RocheSans-Italic.ttf"),
-         file.path(font_dir, "RocheSans-BoldItalic.ttf")
+font_add(
+  "RocheSans",
+  file.path(font_dir, "RocheSans-Regular.ttf"),
+  file.path(font_dir, "RocheSans-Bold.ttf"),
+  file.path(font_dir, "RocheSans-Italic.ttf"),
+  file.path(font_dir, "RocheSans-BoldItalic.ttf")
 )
 
 # Create the weighted least squares equation
@@ -30,20 +31,22 @@ temp_dir <- tempdir(TRUE)
 tex_file_path <- file.path(temp_dir, "StatEng_eqn.tex")
 
 cat(
-"\\documentclass[preview]{standalone}
+  "\\documentclass[preview]{standalone}
 \\begin{document}
 $(X^TWX)\\hat{\\beta} = X^TWy$
 \\end{document}
 ",
-file = tex_file_path)
+  file = tex_file_path
+)
 out_pdf <- tinytex::latexmk(tex_file_path)
-system(paste("pdftocairo -svg", out_pdf))
-eqn_pic <- grImport2::readPicture(file.path(temp_dir, "StatEng_eqn.svg"))
-eqn_gtree <- grid::gTree(children=gList(pictureGrob(eqn_pic)))
+out_svg <- file.path(temp_dir, "StatEng_eqn.svg")
+system(paste("pdftocairo -svg", out_pdf, out_svg))
+eqn_pic <- grImport2::readPicture(out_svg)
+eqn_gtree <- grid::gTree(children = gList(pictureGrob(eqn_pic)))
 
 
 # Create the "bridge"
-parabola <- function(x, a = 1, b = 0, c = 0)  a * x^2 + b * x + c
+parabola <- function(x, a = 1, b = 0, c = 0) a * x^2 + b * x + c
 s1 <- seq(-0.5, 1, length = 10)
 s2 <- seq(1, 0, length = 10)
 s3 <- seq(0, 1, length = 10)
@@ -51,7 +54,7 @@ s4 <- seq(1, -0.5, length = 10)
 
 bridge_data <-
   data.frame(
-    x = c(seq(-5,19, length = 10), seq(21.5, 78.5, length = 20), seq(81, 105, length = 10)),
+    x = c(seq(-5, 19, length = 10), seq(21.5, 78.5, length = 20), seq(81, 105, length = 10)),
     y = c(
       s1,
       parabola(s2),
@@ -62,17 +65,17 @@ bridge_data <-
   )
 
 feature_color <- "#1881C2"
-bridge_fill <-  "#BFDBED"
+bridge_fill <- "#BFDBED"
 
 bridge_plot <- ggplot(bridge_data, aes(x = x, y = y, weight = exp(w))) +
   stat_smooth(method = "lm", formula = y ~ x, col = feature_color, fill = bridge_fill, se = TRUE, fullrange = TRUE) +
-  geom_point(aes(size = w^5), show.legend = FALSE, color =  feature_color, shape = 16, alpha = 0.75) +
+  geom_point(aes(size = w^5), show.legend = FALSE, color = feature_color, shape = 16, alpha = 0.75) +
   annotate("path", x = c(20, 20), y = c(-1, 1), linetype = 2) +
   annotate("path", x = c(80, 80), y = c(-1, 1), linetype = 2) +
   ylim(-1, 2) +
   xlim(-10, 110) +
   theme_void() +
-  annotation_custom(eqn_gtree, xmin=16, xmax=84, ymin=-1, ymax=-0.2)
+  annotation_custom(eqn_gtree, xmin = 16, xmax = 84, ymin = -1, ymax = -0.2)
 
 
 # export SVG
@@ -91,12 +94,11 @@ sticker(
   s_height = 1.2,
   h_fill = "#FFFFFF",
   h_color = "#1881C2",
-  filename = "StatisticalEngineering.svg",
+  filename = "SVG/StatisticalEngineering.svg",
   dpi = 300
 )
 
 image_write(
-  image_read_svg("StatisticalEngineering.svg", width = 1200),
-  path = "StatisticalEngineering.png"
-  )
-
+  image_read_svg("SVG/StatisticalEngineering.svg", height = 1200),
+  path = "PNG/StatisticalEngineering.png"
+)
